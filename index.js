43ed6fd26d581d632d40c04d15e4edffe9cbce38
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-06-16 12:26:46 -0700

    Completed task 1 with class

--- index.js
+++ index.js
@@ -39,15 +39,38 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
  
- 
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    }
+  }
+  Person.prototype.poop = function(){
+    this.stomach = [];
+  }
 
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`;
+  }
   
-  
-  
+  const romy = new Person('Romy', 29);
+  const bradly = new Person('Bradly', 27);
+  const jordan = new Person('Jordan', 24);
+  console.log(romy);
+  console.log(bradly);
+  console.log(jordan);
+
+  jordan.eat("pizza");
+  jordan.eat("tacos")
+  console.log(jordan.stomach);
+  jordan.poop();
+  console.log(jordan.stomach);
+  console.log(jordan.toString());
   
   /*
     TASK 2
